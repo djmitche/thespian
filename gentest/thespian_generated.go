f0e84d1f2ca7cbb5744f6519445d86b6bc673d30
Dustin J. Mitchell <dustin.mitchell@datadoghq.com>
2021-11-18 17:10:21 +0000

    mailbox ideas

--- gentest/thespian_generated.go
+++ gentest/thespian_generated.go
@@ -4,59 +4,6 @@ package gentest
 
 import "github.com/djmitche/thespian"
 
-// --- Reporter
-
-// Reporter is the public handle for reporter actors.
-type Reporter struct {
-	stopChan   chan<- struct{}
-	reportChan chan<- []string
-}
-
-// Stop sends a message to stop the actor.
-func (a *Reporter) Stop() {
-	a.stopChan <- struct{}{}
-}
-
-// Report sends the Report message to the actor.
-func (a *Reporter) Report(m []string) {
-	a.reportChan <- m
-}
-
-// --- reporter
-
-func (a reporter) spawn(rt *thespian.Runtime) *Reporter {
-	rt.Register(&a.ActorBase)
-	handle := &Reporter{
-		stopChan:   a.StopChan,
-		reportChan: a.reportChan,
-	}
-	go a.loop()
-	return handle
-}
-
-func (a *reporter) loop() {
-	defer func() {
-		a.cleanup()
-	}()
-
-	a.HandleStart()
-	for {
-		select {
-		case <-a.HealthChan:
-			// nothing to do
-		case <-a.StopChan:
-			a.HandleStop()
-			return
-		case m := <-a.reportChan:
-			a.handleReport(m)
-		}
-	}
-}
-
-func (a *reporter) cleanup() {
-	a.Runtime.ActorStopped(&a.ActorBase)
-}
-
 // --- Aggregator
 
 // Aggregator is the public handle for aggregator actors.
@@ -110,5 +57,58 @@ func (a *aggregator) loop() {
 
 func (a *aggregator) cleanup() {
 	a.flushTimer.Stop()
+	a.Runtime.ActorStopped(&a.ActorBase)
+}
+
+// --- Reporter
+
+// Reporter is the public handle for reporter actors.
+type Reporter struct {
+	stopChan   chan<- struct{}
+	reportChan chan<- []string
+}
+
+// Stop sends a message to stop the actor.
+func (a *Reporter) Stop() {
+	a.stopChan <- struct{}{}
+}
+
+// Report sends the Report message to the actor.
+func (a *Reporter) Report(m []string) {
+	a.reportChan <- m
+}
+
+// --- reporter
+
+func (a reporter) spawn(rt *thespian.Runtime) *Reporter {
+	rt.Register(&a.ActorBase)
+	handle := &Reporter{
+		stopChan:   a.StopChan,
+		reportChan: a.reportChan,
+	}
+	go a.loop()
+	return handle
+}
+
+func (a *reporter) loop() {
+	defer func() {
+		a.cleanup()
+	}()
+
+	a.HandleStart()
+	for {
+		select {
+		case <-a.HealthChan:
+			// nothing to do
+		case <-a.StopChan:
+			a.HandleStop()
+			return
+		case m := <-a.reportChan:
+			a.handleReport(m)
+		}
+	}
+}
+
+func (a *reporter) cleanup() {
 	a.Runtime.ActorStopped(&a.ActorBase)
 }
