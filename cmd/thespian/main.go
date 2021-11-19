d0ee2b3ae97e901d2fe9006dc3f0a8373c43b751
Dustin J. Mitchell <dustin.mitchell@datadoghq.com>
2021-11-19 21:13:37 +0000

    remove cobra

--- cmd/thespian/main.go
+++ cmd/thespian/main.go
@@ -4,28 +4,11 @@ import (
 	"log"
 
 	"github.com/djmitche/thespian/internal/gen"
-	"github.com/spf13/cobra"
 )
-
-var rootCmd = &cobra.Command{
-	Use:   "thespian",
-	Short: "Thespian code-generation tool",
-}
-
-var generateCmd = &cobra.Command{
-	Use:   "generate [flags]",
-	Short: "Generate type definitions based on thespian.yml in this directory",
-	Args:  cobra.ExactArgs(0),
-	Run: func(cmd *cobra.Command, args []string) {
-		gen.Generate()
-	},
-}
 
 func main() {
 	log.SetFlags(0)
 	log.SetPrefix("thespian: ")
 
-	rootCmd.AddCommand(generateCmd)
-
-	cobra.CheckErr(rootCmd.Execute())
+	gen.Generate()
 }
--- go.mod
+++ go.mod
@@ -4,8 +4,9 @@ go 1.15
 
 require (
 	github.com/iancoleman/strcase v0.2.0
-	github.com/spf13/cobra v1.2.1
+	github.com/kr/pretty v0.1.0 // indirect
 	golang.org/x/sys v0.0.0-20211117180635-dee7805ff2e1 // indirect
 	golang.org/x/tools v0.1.7
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v2 v2.4.0
 )
